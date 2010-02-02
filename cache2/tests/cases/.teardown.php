0f874b680a69220a54004aaa5453fd6c467378fa
Slevin <RomaKitaev@gmail.com>
2010-02-02 16:38:10 +0300

    -- remove all not used limb packages

--- cache2/tests/cases/.teardown.php
+++
@@ -1,2 +0,0 @@
-<?php
-try { lmbFs :: rm(lmb_var_dir()); } catch(lmbFsException $e) {}
--- imagekit/tests/cases/im/.skipif.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!extension_loaded('imagick'))
-{
-  echo "Imagick library tests are skipped since Imagick extension is disabled.\n";
-  return true;
-}
-return false;
-
