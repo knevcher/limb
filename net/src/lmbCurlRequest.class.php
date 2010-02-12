c665df41d858a195a65534b982cc9b3c027dda25
Slevin <RomaKitaev@gmail.com>
2010-02-12 11:22:41 +0300

    -- lmbCurlRequest code refactoring. can set url and timeout now.

--- net/src/lmbCurlRequest.class.php
+++ net/src/lmbCurlRequest.class.php
@@ -15,16 +15,32 @@
  */
 class lmbCurlRequest
 {
-  protected $url = '';
-  protected $handle;
-  protected $opts = array();
+  protected $_url = '';
+  protected $_handle;
+  protected $_opts = array();
+  
+  protected $_error;
+  protected $_request_status;
 
-  function __construct($url)
+  function __construct($url = '')
   {
-    $this->url = $url;
+    if($url)
+      $this->setUrl($url);
+    
     $this->_initDefaultOptions();
   }
-
+  
+  function setUrl($url)
+  {
+    $this->_url = $url;
+    $this->setOpt(CURLOPT_URL, $url);
+  }
+  
+  function setTimeout($timeout)
+  {
+    $this->setOpt(CURLOPT_TIMEOUT_MS, $timeout); 
+  }
+  
   function open($post_data = '')
   {
     $this->_ensureCurl();
@@ -37,40 +53,47 @@ class lmbCurlRequest
 
   function setOpt($opt, $value)
   {
-    $this->opts[$opt] = $value;
+    $this->_opts[$opt] = $value;
   }
 
   protected function _initDefaultOptions()
   {
-    $this->opts = array(CURLOPT_HEADER => 0,
-                        CURLOPT_RETURNTRANSFER => 1,
-                        CURLOPT_URL => $this->url);
+    $this->_opts = array(
+      CURLOPT_HEADER => 0,
+      CURLOPT_RETURNTRANSFER => 1,
+    );
   }
 
   protected function _ensureCurl()
   {
-    if(!is_resource($this->handle))
-     $this->handle = curl_init();
+    if(!is_resource($this->_handle))
+      $this->_handle = curl_init();
   }
 
   protected function _setupCurlOptions()
   {
-    foreach($this->opts as $opt => $value)
-      curl_setopt($this->handle, $opt, $value);
+    foreach($this->_opts as $opt => $value)
+      curl_setopt($this->_handle, $opt, $value);
   }
 
   protected function _exec()
   {
-    $res = curl_exec($this->handle);
+    $res = curl_exec($this->_handle);
+    
+    $this->_request_status = curl_getinfo($this->_handle, CURLINFO_HTTP_CODE);
+    $this->_error = curl_error($this->_handle);
+    
     $this->_resetCurl();
+    
     return $res;
   }
 
   protected function _resetCurl()
   {
-    if(is_resource($this->handle))
-      curl_close($this->handle);
-    $this->opts = array();
+    if(is_resource($this->_handle))
+      curl_close($this->_handle);
+    
+    $this->_opts = array();
   }
 
   protected function _setPostData($post_data)
@@ -91,6 +114,16 @@ class lmbCurlRequest
         $var_string .= "{$k}={$v}&";
 
     $this->setOpt(CURLOPT_POSTFIELDS, $var_string);
+  }
+  
+  function getError()
+  {
+    return $this->_error;
+  }
+
+  function getRequestStatus()
+  {
+    return $this->_request_status;
   }
 }
 
