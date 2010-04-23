d4eae1be2cc930c949c7d101506f71c983136e7e
romakitaev <romakitaev@gmail.com>
2010-04-23 17:48:40 +0400

    class lmbCurlRequest can now do PUT and DELETE requests

--- net/src/lmbCurlRequest.class.php
+++ net/src/lmbCurlRequest.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 
 /**
@@ -18,7 +18,7 @@ class lmbCurlRequest
   protected $_url = '';
   protected $_handle;
   protected $_opts = array();
-  
+
   protected $_error;
   protected $_request_status;
 
@@ -26,29 +26,73 @@ class lmbCurlRequest
   {
     if('' != $url)
       $this->setUrl($url);
-    
+
     $this->_initDefaultOptions();
   }
-  
+
   function setUrl($url)
   {
     $this->_url = $url;
     $this->setOpt(CURLOPT_URL, $url);
   }
-  
+
   function setTimeout($timeout)
   {
-    $this->setOpt(CURLOPT_TIMEOUT_MS, $timeout); 
+    $this->setOpt(CURLOPT_TIMEOUT_MS, $timeout);
   }
-  
-  function open($post_data = null)
+
+  function open($data = null, $request_method = null)
   {
     $this->_ensureCurl();
-    if(!is_null($post_data))
-      $this->_setPostData($post_data);
+
+    //-- this operation is added for backward compatibility
+    if(!is_null($data) && is_null($request_method))
+      $this->_setupPostRequest($data);
+
+    else if('POST' == $request_method)
+      $this->_setupPostRequest($data);
+
+    else if('PUT' == $request_method)
+      $this->_setupPutRequest($data);
+
+    else if('DELETE' == $request_method)
+      $this->_setupDeleteRequest();
 
     $this->_setupCurlOptions();
+
     return $this->_exec();
+  }
+
+  protected function _setupPostRequest($data)
+  {
+    $this->setOpt(CURLOPT_POST, 1);
+
+    if(!is_null($data))
+      $this->setOpt(CURLOPT_POSTFIELDS, $data);
+  }
+
+  protected function _setupPutRequest($data)
+  {
+    $data_str = "";
+    if(!is_null($data))
+    {
+      foreach($data as $key => $value)
+        $data_str .= "$key=$value&";
+    }
+
+    $fh = fopen('php://memory', 'rw');
+    fwrite($fh, $data_str);
+    rewind($fh);
+
+    $this->setOpt(CURLOPT_PUT, 1);
+    $this->setOpt(CURLOPT_INFILE, $fh);
+    $this->setOpt(CURLOPT_INFILESIZE, strlen($data_str));
+    $this->setOpt(CURLOPT_HTTPHEADER, Array('Expect: '));
+  }
+
+  protected function _setupDeleteRequest()
+  {
+    $this->setOpt(CURLOPT_CUSTOMREQUEST, 'DELETE');
   }
 
   function setOpt($opt, $value)
@@ -79,12 +123,12 @@ class lmbCurlRequest
   protected function _exec()
   {
     $res = curl_exec($this->_handle);
-    
+
     $this->_request_status = curl_getinfo($this->_handle, CURLINFO_HTTP_CODE);
     $this->_error = curl_error($this->_handle);
-    
+
     $this->_resetCurl();
-    
+
     return $res;
   }
 
@@ -92,16 +136,10 @@ class lmbCurlRequest
   {
     if(is_resource($this->_handle))
       curl_close($this->_handle);
-    
+
     $this->_opts = array();
   }
 
-  protected function _setPostData($post_data)
-  {
-    $this->setOpt(CURLOPT_POST, 1);
-    $this->setOpt(CURLOPT_POSTFIELDS, $post_data);
-  }
-  
   function getError()
   {
     return $this->_error;
