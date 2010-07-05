1707e14d49c114a6aa6a9d1dd957285dfaba8f3b
knevcher <knevcher@gmail.com>
2010-07-05 11:23:48 +0400

    -- security fix in lmbFs package

--- fs/src/lmbFileLocator.class.php
+++ fs/src/lmbFileLocator.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/fs/src/exception/lmbFileNotFoundException.class.php');
 lmb_require('limb/fs/src/lmbFs.class.php');
@@ -26,6 +26,8 @@ class lmbFileLocator
 
   function locate($alias, $params = array())
   {
+    if(false !== strpos($alias, "\0"))
+      return false;
     if(lmbFs :: isPathAbsolute($alias))
     {
        if(file_exists($alias))
